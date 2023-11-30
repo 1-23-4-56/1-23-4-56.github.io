e7eeb5434deb17f3b20a0f805ceeca8e00de07f0
1-23-4-56 <152521093+1-23-4-56@users.noreply.github.com>
2023-11-30 18:46:06 +0100

    change 1

---
+++ main.js
@@ -0,0 +1,88 @@
+function setup() {
+  creditsHeight = creditsMultiplier * windowWidth * windowHeight / 1010000;
+
+  createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+  creditsHeight = creditsMultiplier * windowWidth * windowHeight / 1010000;
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+var ifCircle = false;
+
+var creditsMultiplier = 20;
+var creditsHeight = 20;
+
+function draw() {
+  background(220);
+
+  drawBorderRects();
+  drawMouseCircle();
+
+  writeCredits();
+}
+
+function mousePressed() {
+  ifCircle = !ifCircle;
+
+  if (mouseX > (width - 50)) {
+    creditsMultiplier++;
+    windowResized();
+  }
+  if (mouseX < 50) {
+    creditsMultiplier--;
+    windowResized();
+  }
+}
+
+function drawMouseCircle() {
+  push();
+
+  noStroke();
+  if (ifCircle) {
+    fill(0, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+  }
+  else {
+    rectMode(CENTER);
+    fill(255, 0, 0);
+    rect(mouseX, mouseY, 50, 50);
+  }
+
+  pop();
+}
+
+function drawBorderRects() {
+  push();
+
+  fill(0, 0, 255);
+  stroke(255, 0, 0);
+  rect(0, 0, 50, 50);
+
+  fill(0, 255, 255);
+  rect(width - 50, 0, 50, 50);
+
+  fill(255, 0, 255);
+  rect(0, height - 50, 50, 50);
+
+  fill(0, 255, 0);
+  rect(width - 50, height - 50, 50, 50);
+
+  pop();
+}
+
+function writeCredits() {
+  push();
+
+  stroke(0);
+  textSize(creditsHeight);
+  textAlign(RIGHT, BOTTOM);
+
+  text("Creato da:\nLuca Lazzaroni - 3°CT", width - 10, height - 10);
+  //################
+  text(Math.trunc(millis()) + "\t" + creditsMultiplier, width / 2, height / 2);
+  //################
+
+  pop();
+}
