93d905b1b4463051177820fa80a1dc7a2877d38d
1-23-4-56 <152521093+1-23-4-56@users.noreply.github.com>
2023-12-04 18:39:25 +0100

    test for phone and tensorflow

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let model;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+var loadedstate = false;
 
-  testPredict();
+async function setup() {
+  await loadTheModel();
+  loadedstate = await testTheModel();
+
+  createCanvas(windowWidth, windowHeight);
 }
 
 function windowResized() {
@@ -12,14 +15,28 @@ function windowResized() {
 
 function draw() {
   background(220);
+
+  text(loadedstate, 100, 100)
+}
+
+async function mousePressed() {
+  const inputImage = document.getElementById('testImage');
+  const [result, probClass0, probClass1, probClass2, probClass3, probClass4, probClass5, probClass6, probClass7, probClass8, probClass9] = await runPrediction(model, inputImage);
+
+  console.log('thisguyshouldbeathreee', result)
 }
 
 //######################################
 
-async function testPredict() {
+async function loadTheModel() {
   model = await tf.loadLayersModel('modelJS/model.json');
+  console.log('model loaded');
+}
+
+async function testTheModel() {
   const inputImage = document.getElementById('testImage');
   const [result, probClass0, probClass1, probClass2, probClass3, probClass4, probClass5, probClass6, probClass7, probClass8, probClass9] = await runPrediction(model, inputImage);
 
-  console.log('ris', result);
+  console.log('test result:', result == 3);
+  return result == 3;
 }
